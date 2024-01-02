115302e5cd7f56807e8cf4ea1a7ea632b6fbbbcb
Christopher Lamm <christopher.lamm@companycam.com>
2024-01-02 09:08:25 -0500

    created custom initializer for controller

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        
+        super.init(coder: coder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
